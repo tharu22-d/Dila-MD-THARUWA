34003e2bce1c6ca27f1d944864995d6d09626bd6
tharu22-d <dehemitharusha37@gmail.com>
2024-09-17 16:54:40 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "aOBw1RRI#RqHgeMEgUw3rG_8JkiX2NcfW6950m71tTJ066O7W5ZU",
 MONGODB: process.env.MONGODB || "mongodb://mongo:PDkOJxKWbVHUcNOvwdemPNqrAHlGCiZt@autorack.proxy.rlwy.net:57894",
 };
